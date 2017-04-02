a901dad926bbc0f95fd08fc23c8b5ec9411e159c
Rina <rinaattieh@icloud.com>
2017-04-02 20:10:46 +0200

    Layout

--- app/src/main/java/com/example/rinaattieh/android_sport_projet/MainActivity.java
+++ app/src/main/java/com/example/rinaattieh/android_sport_projet/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.rinaattieh.android_sport_projet;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,7 +17,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -23,9 +27,12 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     Spinner spinner;
+    Button go =null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -37,6 +44,18 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+
+            }
+        });
+
+        go = (Button) findViewById(R.id.go);
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goo = new Intent(getApplicationContext(), Go.class);
+                startActivity(goo);
             }
         });
 
@@ -69,6 +88,7 @@ public class MainActivity extends AppCompatActivity
 
 //////////////////////////// SPINNER /////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -119,10 +139,17 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.new_match) {
-            // Handle the camera action
+            Intent newAct = new Intent(getApplicationContext(), NewMatch.class);
+            startActivity(newAct);
+
         } else if (id == R.id.stat) {
+            Intent newAct = new Intent(getApplicationContext(), Stat.class);
+            startActivity(newAct);
+
 
         } else if (id == R.id.players) {
+            Intent newAct = new Intent(getApplicationContext(), Players.class);
+            startActivity(newAct);
 
         } else if (id == R.id.nav_camera) {
 
---
+++ app/src/main/java/com/example/rinaattieh/android_sport_projet/Stat.java
@@ -0,0 +1,13 @@
+package com.example.rinaattieh.android_sport_projet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Stat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stat);
+    }
+}
