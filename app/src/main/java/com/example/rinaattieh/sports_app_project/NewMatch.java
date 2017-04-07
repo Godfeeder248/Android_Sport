047d3ece529d831e0e3c9703ef604b1df499ff8f
Rina <rinaattieh@icloud.com>
2017-04-07 12:27:21 +0200

    Test mise en place map

--- app/src/main/java/com/example/rinaattieh/sports_app_project/NewMatch.java
+++ app/src/main/java/com/example/rinaattieh/sports_app_project/NewMatch.java
@@ -1,13 +1,30 @@
 package com.example.rinaattieh.sports_app_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewMatch extends AppCompatActivity {
+
+    Button btn_map = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_match);
+
+
+        btn_map = (Button) findViewById(R.id.btn_map);
+
+        btn_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int_map = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(int_map);
+            }
+        });
     }
+
 }
